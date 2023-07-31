c998721cb29b71285036296d99fd0a95e5606f87
ferleyrincon <heiner.rincon@urosario.edu.co>
2023-07-31 07:26:07 -0500

    survey
    initial questions

--- survey/models.py
+++ survey/models.py
@@ -31,15 +31,30 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
+    p_age = models.IntegerField(label="1. Edad")
+
     p_sex = models.IntegerField(
     choices=[
         [0, 'Hombre'],
         [1, 'Mujer'],
-        [2, 'Otro'],
-    ], label="1. ¿Cuál es su sexo?")
+        [2, 'No binario'],
+    ], label="2. ¿Con qué género se identifica?")
 
-    p_age = models.IntegerField(label="2. Edad")
+    p_married = models.IntegerField(
+    choices=[
+        [1, 'Unión libre'],
+        [2, 'Casado (a)'],
+        [3, 'Viudo (a)'],
+        [4, 'Soltero (a)'],
+        [5, 'Divorciado (a)/Separado(a)'],
+    ], label="3. Estado civil")
+    
+    p_family = models.IntegerField(min=0, max=20, label="4. ¿Número de miembros del hogar? (Puede escribir '1' si vive solo)")
 
+    p_children = models.IntegerField(min=0, max=20, label="5. ¿Número de hijos? (Puede escribir '0' si no tiene hijos)")
+
+    p_workers = models.IntegerField(min=0, max=20, label="6. ¿Número de personas que contribuyen a los ingresos del hogar? (Puede escribir '1' si solo una persona trabaja)")
+  
     p_educ = models.IntegerField(
     choices=[
         [1,'Ninguno'],
@@ -48,7 +63,7 @@ class Player(BasePlayer):
         [4,'Técnico o Tecnólogo'],
         [5,'Pregrado'],
         [6,'Posgrado (Especialización, Maestría, Doctorado)']
-    ], label="3. ¿Cuál es el nivel educativo más alto que cursó o está cursando?")
+    ], label="7. ¿Cuál es el nivel educativo más alto que cursó o está cursando?")
 
     p_job = models.IntegerField(
     choices=[
@@ -61,7 +76,7 @@ class Player(BasePlayer):
         [7,'Retirado/pensionado'],
         [8,'Otro'],
         [9,'No sabe']
-    ], label="4. ¿Cuál es su situación laboral actual?")
+    ], label="8. ¿Cuál es su situación laboral actual?")
 
     p_inc = models.IntegerField(
     choices=[
@@ -71,22 +86,11 @@ class Player(BasePlayer):
         [4,'Entre $ 900.000 - $ 1.200.000'],
         [5,'Entre $ 1.200.000 - $ 1.500.000'],
         [6,'Mayor a $ 1.500.000'],
-    ], label="5.1 ¿Cuál es el rango de su ingreso mensual?")
+    ], label="9. ¿Cuál es el rango de su ingreso mensual?")
 
-    p_inc1 = models.CurrencyField(min=c(0), max=c(100000000), label="5.2. ¿Cuánto es su ingreso mensual?")
+    p_inc1 = models.CurrencyField(min=c(0), max=c(100000000), label="10. ¿Cuánto es su ingreso mensual?")
 
-    p_married = models.IntegerField(
-    choices=[
-        [1, 'Unión libre'],
-        [2, 'Casado (a)'],
-        [3, 'Viudo (a)'],
-        [4, 'Soltero (a)'],
-        [5, 'Divorciado (a)/Separado(a)'],
-    ], label="6. Estado civil")
-
-    p_children = models.IntegerField(min=0, max=20, label="7. Número de hijos (Puede escribir '0' si no tiene hijos)")
-
-    ocu_father = models.IntegerField(
+    p_ocu = models.IntegerField(
     choices=[
         [1,'Empleado con cargo como director o gerente general'],
         [2,'Empleado de nivel auxiliar o administrativo'],
@@ -101,7 +105,7 @@ class Player(BasePlayer):
         [11,'Pensionado'],
         [12,'Desempleado'],
         [13,'Otra actividad u ocupación'],
-    ], label="8. ¿Cuál es su ocupación u oficio?")
+    ], label="11. ¿Cuál es su ocupación u oficio?")
 
 #    p_risk = models.IntegerField(min=0, max=10, label="9. En una escala del 0 al 10, donde: 0 - significa que no esta dispuesto(a) a tomar riesgos en lo absoluto 10 - significa que esta muy dispuesto(a) a tomar riesgos  Por favor, díga, en general ¿Qué tan dispuesto (a) está o no está usted a tomar riesgos en decisiones financieras?")
 
